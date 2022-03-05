169463e9e489820ec61c65868730d148be3f94a6
ananya <ananyapalimar01@gmail.com>
2022-03-05 12:22:48 +0530

    added new file

---
+++ firebase/src/main/java/com/example/demo/CRUD.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class CRUD {
+	private String id;
+	private String username;
+	private String password;
+	private String emailid;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmailid() {
+		return emailid;
+	}
+	public void setEmailid(String emailid) {
+		this.emailid = emailid;
+	}
+
+}
